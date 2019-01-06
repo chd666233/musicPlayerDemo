25436dee210561f4c9c89fd54fafde69dedcc8b2
chd <chd666233@163.com>
2019-01-07 02:22:24 +0800

    准备做进度条 ready to finish progressBar

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ server.listen(3000,()=>{
 server.use(express.static('public'));
 
 server.get('/next',(req,res)=>{
-  fs.readFile('public/lrc/东西（Cover：林俊呈） - 你的叽叽.lrc',(err,data)=>{
+  //Rolling star.lrc   东西（Cover：林俊呈） - 你的叽叽.lrc  그 남자의 거짓말 - 泰仁.lrc
+  fs.readFile('public/lrc/Rolling star.lrc',(err,data)=>{
     if(err) throw err;
     var content=data.toString();
     res.send(content);
