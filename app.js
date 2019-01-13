7e11b9a7cc641946e658316f572d0e02c70cc5de
chd <chd666233@163.com>
2019-01-14 01:49:04 +0800

    播放器进度条优化2

--- app.js
+++ app.js
@@ -8,14 +8,14 @@ server.use(express.static('public'));
 
 server.get('/next',(req,res)=>{
   //Rolling star.lrc   东西（Cover：林俊呈） - 你的叽叽.lrc  그 남자의 거짓말 - 泰仁.lrc
-  fs.readFile('public/lrc/Rolling star.lrc',(err,data)=>{
+  fs.readFile('public/lrc/그 남자의 거짓말 - 泰仁.lrc',(err,data)=>{
     if(err) throw err;
     var content=data.toString();
     res.send(content);
   })
 })
 server.get('/prev',(req,res)=>{
-  fs.readFile('public/lrc/李学仕 - 直觉.lrc',(err,data)=>{
+  fs.readFile('public/lrc/东西（Cover：林俊呈） - 你的叽叽.lrc',(err,data)=>{
     if(err) throw err;
     var content=data.toString();
     res.send(content);
